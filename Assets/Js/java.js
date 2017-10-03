bf226a49e192eedf8101d8ae1e5b50936c4598b7
Jose Alfredo P.C <32446743+VMoebot@users.noreply.github.com>
2017-10-02 23:02:05 -0500

    Variables mostradas en consola con formato

--- Assets/Js/java.js
+++ Assets/Js/java.js
@@ -4,12 +4,13 @@ function Empezarjuego()
     alert("funciona");
     var num1 =Number(ramdom(num1));
     var num2 =Number(ramdom(num2));
+    
     var resultado1= num1+num2;
     var resultadoN=0;
     var estado=0;
     var tiradas=1;
     //rulstado
-    console.log("tu primera tirada fue = "+resultado1);
+   console.log("tu tirada "+tiradas+" es :"+num1+" + "+num2+"="+resultado1);
     //primer ciclo de comparacion
     if(resultado1==7 || resultado1==11)
     {
@@ -24,10 +25,10 @@ function Empezarjuego()
         while(estado==0) //si estado no cambia seguira tirando dados
         {
             tiradas++; //contador de tiradas
-            num1 =Number(ramdom(num1)); 
+            num1 =Number(ramdom(num1));
             num2 =Number(ramdom(num2));
             resultadoN=num1+num2;//resultado de la tirada
-            console.log("tu tirada "+tiradas+" es ="+resultadoN);
+            console.log("tu tirada "+tiradas+" es :"+num1+" + "+num2+"="+resultadoN);
             if(resultado1==resultadoN) //si la tirada actual es Igual a la primera cambia el estado
             {
                 estado=1;       
